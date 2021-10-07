fcafe9673558e6faadc8d08d3d022e6be039c8fa
yacamoz <88639890+yacamoz@users.noreply.github.com>
2021-10-07 16:47:34 +0900

    IBIS 스크래핑 완료
    각 산업별 2011~2021년 까지의 규모를 파싱하는데 성공 목표달성

--- IBISsegmentparsing.py
+++ IBISsegmentparsing.py
@@ -1,4 +1,3 @@
-from typing import Literal
 import requests
 import csv
 from selenium import webdriver
@@ -7,28 +6,34 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
-openfile = open("ibistest.txt","r")
-brower = webdriver.Chrome('C:\pythonnori\PPAP\python\chromedriver.exe')
+options = webdriver.ChromeOptions()
+options.headless = True
 
-list = ["marketname",2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
-list2 = []
-
-filename = "IBIS_practice.csv"
+openfile = open("IBIS_global_market_list.txt","r")
+filename = "IBIS_market_sizes.csv"
 f = open(filename, "w", encoding="utf8", newline="")
 writer = csv.writer(f)
-writer.writerow(list)
-url=openfile.readline()
-brower.get(url)
-try:
-    elem = WebDriverWait(brower, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="Statistics_MarketSize_Chart_Div"]/div/div[1]/div/div/table')))
-    elem = brower.page_source
-    soup = BeautifulSoup(elem, 'html.parser')
-    for size in soup.find_all("td")[:22]:
-        if 'Feb 1' in size.get_text():
-            continue
-        # print(size.get_text())
-        list2.append(size.get_text())  
-    writer.writerow(list2)
-finally:
-    brower.quit()
+list1 = ["marketname",2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
+writer.writerow(list1)
+while True:
+    list2 = []
+    url=openfile.readline()
+    if not url:
+        break
+    brower = webdriver.Chrome('C:\pythonnori\PPAP\python\chromedriver.exe')
+    brower.get(url)
+    try:
+        elem = WebDriverWait(brower, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="Statistics_MarketSize_Chart_Div"]/div/div[1]/div/div/table')))
+        elem = brower.page_source
+        soup = BeautifulSoup(elem, 'html.parser')
+        name = soup.find("span", attrs={"id":"reportTitleDisplay_MarketSize"}).text
+        list2.append(name)
+        for size in soup.find_all("td")[:22]:
+            if 'Feb 1' in size.get_text():
+                continue
+            list2.append(size.get_text())  
+        writer.writerow(list2)
+    finally:
+        brower.quit()
+        list2 = []
 openfile.close()
