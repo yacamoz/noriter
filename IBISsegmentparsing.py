5680148e8a0bf9dc7c74a68392dc134417790ac3
yacamoz <88639890+yacamoz@users.noreply.github.com>
2021-10-07 14:48:24 +0900

    파일에 있는 URL 주소를 불러와서 로딩
    멍청하게 CSV 파일에 있는걸 그대로 박아 넣으려고 아웅다웅 하다가 전혀 안되다가 우연히 떠오른 txt로 변환하면 어떨까하는 생각에 한 결과 성공

--- IBISsegmentparsing.py
+++ IBISsegmentparsing.py
@@ -1,3 +1,4 @@
+from typing import Literal
 import requests
 import csv
 from selenium import webdriver
@@ -6,18 +7,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
+openfile = open("ibistest.txt","r")
 brower = webdriver.Chrome('C:\pythonnori\PPAP\python\chromedriver.exe')
 
 list = ["marketname",2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
 list2 = []
 
-filename = "IBIS_global_industry_marketsize.csv"
+filename = "IBIS_practice.csv"
 f = open(filename, "w", encoding="utf8", newline="")
 writer = csv.writer(f)
 writer.writerow(list)
-url="https://www.ibisworld.com/global/market-size/global-advertising-agencies/"
+url=openfile.readline()
 brower.get(url)
-idx=1
 try:
     elem = WebDriverWait(brower, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="Statistics_MarketSize_Chart_Div"]/div/div[1]/div/div/table')))
     elem = brower.page_source
@@ -28,6 +29,6 @@ try:
         # print(size.get_text())
         list2.append(size.get_text())  
     writer.writerow(list2)
-
 finally:
     brower.quit()
+openfile.close()
