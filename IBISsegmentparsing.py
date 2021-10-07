54bd063f7da8a2fff31848ff6c0fbeb9f15e1edc
yacamoz <88639890+yacamoz@users.noreply.github.com>
2021-10-07 10:42:57 +0900

    시장규모 한 행 출력 성공
    기존 코드로 실행시 CSV 파일이 한 열로 정렬되어서 출력되는 문제가 발생. 해결하기 위해 writer를 변화시키려 했지만 실패.
    대신 리스트의 경우 한꺼번에 출력된다는 점을 생각하여 리스트로 묶어서 출력하는 방법을 강구하여 출력한 결과 성공.
    추가적으로 market name에 들어가도록 해당 요소를 파싱하도록 하여 추가하고 CSV 파일을 읽어서 URL을 불러오도록 할 것.

--- IBISsegmentparsing.py
+++ IBISsegmentparsing.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,6 +8,13 @@ from bs4 import BeautifulSoup
 
 brower = webdriver.Chrome('C:\pythonnori\PPAP\python\chromedriver.exe')
 
+list = ["marketname",2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
+list2 = []
+
+filename = "IBIS_global_industry_marketsize.csv"
+f = open(filename, "w", encoding="utf8", newline="")
+writer = csv.writer(f)
+writer.writerow(list)
 url="https://www.ibisworld.com/global/market-size/global-advertising-agencies/"
 brower.get(url)
 idx=1
@@ -15,7 +23,11 @@ try:
     elem = brower.page_source
     soup = BeautifulSoup(elem, 'html.parser')
     for size in soup.find_all("td")[:22]:
-        print(size.get_text())
+        if 'Feb 1' in size.get_text():
+            continue
+        # print(size.get_text())
+        list2.append(size.get_text())  
+    writer.writerow(list2)
 
 finally:
     brower.quit()
